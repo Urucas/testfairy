068a12ec1cfdb2dc39ea1346d0520af2c225fa46
vrunoa <vruno.alassia@gmail.com>
2015-09-23 13:54:09 -0300

    add ios uploader class

---
+++ TestFairyIOS.es6.js
@@ -0,0 +1,61 @@
+import TestFairy from './TestFairy.js'
+import glue from 'glue-path'
+import fs from 'fs'
+import child_process from 'child_process'
+
+export default class TestFairyIOS extends TestFairy {
+  
+  constructor(params) {
+    super(params)
+    this.apk_file = params.apk_file;
+    this.is_android = true;
+  }
+
+  validate_min_params() {
+    let logger = this.logger;
+    if(this.verbose)
+      logger.echo("Checkin minimal params are setted")
+    
+    if(this.api_key == undefined)
+      return new Error("Undefined api_key param");
+
+    if(this.verbose)
+      logger.ok("Checking api_key key is not undefined");
+
+    if(this.ios_file == undefined)
+      return new Error("Undefined ios_file param");
+    
+    if(this.verbose)
+      logger.ok("Checking ios_file key is not undefined");
+
+    this.ios_file = glue([process.cwd(), this.ios_file]);
+    try {
+      let err = fs.accessSync(this.ios_file, fs.R_OK);
+      if(err) return err;
+    }catch(e) {
+      return new Error(e.message);
+    }
+    return null;
+  }
+
+  upload() {
+    let logger = this.logger;
+    if(this.verbose) 
+      logger.echo("Uploading apk to TestFairy");
+      
+    let params = [
+      '-s', this.server_endpoint+"/api/upload",
+      '-F', 'api_key='+this.api_key,
+      '-F', 'file=@'+this.ios_file
+    ];
+    let child = child_process.spawnSync("curl", params);
+    let response = child.stdout.toString();
+    try {
+      response = JSON.parse(response);
+      return [null, response];
+    }catch(e) {
+      return [e];
+    }
+  }
+
+}
