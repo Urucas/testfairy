db72620ac8687e47c97b4192f4049def2e70f56d
vrunoa <vruno.alassia@gmail.com>
2015-09-23 00:01:34 -0300

    add uploader classes

--- TestFairy.es6.js
+++ TestFairy.es6.js
@@ -1,23 +1,19 @@
 import request from 'request'
-import './TestFairyAndroid.js';
 const server_endpoint = "http://app.testfairy.com";
 
 export default class TestFairy {
 
   constructor(params) {
-    this.api_key   = params.api_key;
-    this.keystore  = params.keystore;
-    this.storepass = params.storepass;
-    this.alias     = params.alias;
-    this.testers   = params.testers || [];
-    this.notify    = params.notify ? "on" : "off";
-    this.max_duration = params.max_duration || "10m";
-    this.video = params.video ? "on" : "off";
+    this.api_key        = params.api_key;
+    this.notify         = params.notify == "on" ? "on" : "off";
+    this.max_duration   = params.max_duration || "10m";
+    this.video          = params.video == "on" ? "on" : "off";
     this.icon_watermark = params.watermark ? "on" : "off";
-    this.comment = params.comment || "";
+    this.comment        = params.comment || "";
   }
-  
+
   upload() {
+    return null
   }
 
 }
--- TestFairyAndroid.es6.js
+++ TestFairyAndroid.es6.js
@@ -1,4 +1,6 @@
 import TestFairy from './TestFairy.js'
+import glue from 'glue-path'
+import fs from 'fs'
 
 export default class TestFairyAndroid extends TestFairy {
   
@@ -6,5 +8,24 @@ export default class TestFairyAndroid extends TestFairy {
     super(params)
     this.apk_file = params.apk_file;
     this.is_android = true;
+  }
+
+  validate_min_params() {
+    if(this.api_key == undefined)
+      return new Error("Undefined api_key param");
+
+    if(this.apk_file == undefined)
+      return new Error("Undefined apk_file param");
+
+    this.apk_file = glue([process.cwd(), this.apk_file]);
+
+    try {
+      let err = fs.accessSync(this.apk_file, fs.R_OK);
+      if(err) return err;
+    }catch(e) {
+      return new Error(e.message);
+    }
+    
+    return null;
   }
 }
