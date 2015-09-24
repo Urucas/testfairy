82a229b5cc929ef4a93e02488c81cf49f7d426a0
vrunoa <vruno.alassia@gmail.com>
2015-09-23 21:16:51 -0300

    validate platform param is defined

--- TestFairyUploader.es6.js
+++ TestFairyUploader.es6.js
@@ -4,13 +4,18 @@ import TestFairyIOS from './TestFairyIOS.js'
 export default class TestFairyUploader {
 
   constructor(params) {
-    if(params.platform == "android") 
+    if(params == undefined)
+      this.uploader = null;
+    else if(params.platform == "android") 
       this.uploader = new TestFairyAndroid(params)
     else if(params.platform == "ios")
       this.uploader = new TestFairyIOS(params)
+    else this.uploader = null;
   }
 
   run() {
+    if(this.uploader == null) 
+      return [new Error("Undefined uploader params, could not create uploader class")];
     let err = this.uploader.validate_min_params();
     if(err) return [err]
     return this.uploader.upload();
