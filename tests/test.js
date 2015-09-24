30c7c517825503c89ce51211c3bc74f8511a6e1b
vrunoa <vruno.alassia@gmail.com>
2015-09-23 21:24:09 -0300

    add tests

--- tests/test.js
+++ tests/test.js
@@ -18,4 +18,15 @@ describe("Test TestFairyUploader", () => {
       throw new Error("uploader class instance is not TestFairyAndroid, className: "+className);
     done();
   })
+
+  it("uploader should be instance of TestFairyIOS", (done) => {
+    let uploader = new TestFairyUploader({platform:"ios"});
+    if(uploader.uploader == null)
+      throw new Error("uploader class is null and should be instance of TestFairyAndroid");
+    let className = uploader.uploader.constructor.name;
+    if(className != "TestFairyIOS")
+      throw new Error("uploader class instance is not TestFairyIOS, className: "+className);
+    done();
+  })
+
 })
